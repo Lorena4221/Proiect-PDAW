fb5ae37cc4d4359243a8dadb2b97869323795bd9
Lorena4221 <georgiana.galea@student.upt.ro>
2025-05-04 00:10:25 +0300

    login/logout

--- cinemapp/urls.py
+++ cinemapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('add_film/', views.add_film, name='add_film'),
     path('delete/<str:title>/', views.delete_film, name='delete_film'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- cinemapp/views.py
+++ cinemapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 from .forms import FilmForm
 from .models import Film
 
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+@login_required
 def home(request):
     filme = Film.objects.all()
     return render(request, "home.html", {'filme': filme})
@@ -24,5 +32,3 @@ def delete_film(request, title):
         return redirect('add_film')
     except Film.DoesNotExist:
         return HttpResponse("Film not found.")
-
-# Create your views here.
